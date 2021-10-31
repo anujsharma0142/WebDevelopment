c17725e10bb797ad0001196e1283aa2740fe90ef
Parijain25 <73413170+Parijain25@users.noreply.github.com>
2021-10-31 23:09:39 +0530

    Create SimpleQuiz.js

---
+++ SimpleQuiz.js
@@ -0,0 +1,133 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following option leads to the portability and security of Java?",
+    answer: "Bytecode is executed by the JVM",
+    options: [
+      "Bytecode is executed by JVM",
+      "The applet makes the Java code secure and portable",
+      "Use of exception handling",
+      "Dynamic binding between objects"
+    ]
+  },
+    {
+    numb: 2,
+    question: " Which of the following is not a Java features?",
+    answer: "Use of pointers",
+    options: [
+      "Dynamic",
+      "Architecture Neutral",
+      "Use of pointers",
+      "Object-oriented"
+    ]
+  },
+    {
+    numb: 3,
+    question: " ___ is used to find and fix bugs in the Java programs.",
+    answer: "JDB",
+    options: [
+      "JVM",
+      "JRE",
+      "JDK",
+      "JDB"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "What is the return type of the hashCode() method in the Object class?",
+    answer: "INT",
+    options: [
+      "INT",
+      "VOID",
+      "LONG",
+      "OBJECT"
+    ]
+  },
+
+  {  numb: 7,
+    question: "Which of the following tool is used to generate API documentation in HTML format from doc comments in source code?",
+    answer: "Javadoc tool",
+    options: [
+      "javap tool",
+      "javaw command",
+      "Javadoc tool",
+      "javah command"
+    ]
+  },
+
+  {  
+    numb: 8,
+    question: " Which method of the Class.class is used to determine the name of a class represented by the class object as a String?",
+    answer: "getName()",
+    options: [
+      "getName()",
+      "intern()",
+      "tiString()",
+      "getClass()"
+    ]
+  },
+
+  {  
+    numb: 9,
+    question: " What do you mean by nameless objects?",
+    answer: "An object that has no reference.",
+    options: [
+      "An object that has no reference.",
+      "An object of a superclass created in the subclass.",
+      "An object without having any name but having a reference.",
+      "An object created by using the new keyword."
+    ]
+  },
+
+  {  
+    numb: 10,
+    question: " Which of the following is an immediate subclass of the Panel class?",
+    answer: "Applet Class.",
+    options: [
+      "Applet Class.",
+      "Window Class.",
+      "Dialog Class.",
+      "Analog Class."
+    ]
+  },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
